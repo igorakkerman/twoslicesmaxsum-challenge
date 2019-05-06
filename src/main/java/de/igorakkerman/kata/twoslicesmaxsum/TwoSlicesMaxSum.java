faa6fb0224be20c050a19c905e5d5486f04a0148
Igor Akkerman <codingchallenge@igorakkerman.de>
2019-05-06 12:52:41 +0200

    Algorithm improved to have time and space complexity of O(n)

--- src/main/java/de/igorakkerman/kata/twoslicesmaxsum/TwoSlicesMaxSum.java
+++ src/main/java/de/igorakkerman/kata/twoslicesmaxsum/TwoSlicesMaxSum.java
@@ -1,41 +1,40 @@
 package de.igorakkerman.kata.twoslicesmaxsum;
 
+import static java.lang.Math.max;
+
 public class TwoSlicesMaxSum {
     public static int twoSlicesMaxSum(int[] a, int k, int l) {
         if (a.length < k + l)
             return -1;
 
-        int[] kSums = new int[a.length];
-        int[] lSums = new int[a.length];
+        int[] kMaxSums = new int[a.length - l];
 
-        collectSums(a, kSums, k, 0, a.length - (k + l));
-        collectSums(a, lSums, l, k, a.length - l);
-
-        return findMaxSum(a, k, l, kSums, lSums);
+        kSums(a, k, l, kMaxSums);
+        return klSums(a, k, l, kMaxSums);
     }
 
-    private static void collectSums(int[] a, int[] sums, int length, int first, int lastStart) {
-        int sum = sliceSum(a, first, length);
-
-        sums[first] = sum;
-        for (int start = first + 1; start <= lastStart; start++) {
-            sum = sum - a[start - 1] + a[start + length - 1];
-            sums[start] = sum;
+    private static void kSums(int[] a, int k, int l, int[] kMaxSums) {
+        int kSum = sliceSum(a, 0, k - 1);
+        kMaxSums[k - 1] = kSum;
+        for (int end = k; end < a.length - l; end ++) {
+            kSum = kSum - a[end - k] + a[end];
+            kMaxSums[end] = max(kMaxSums[end - 1], kSum);
         }
     }
 
-    private static int findMaxSum(int[] a, int k, int l, int[] kSums, int[] lSums) {
-        int max = 0;
-        for (int i = 0; i <= a.length - (k + l); i++)
-            for (int j = i + k; j <= a.length - l; j++)
-                max = Math.max(kSums[i] + lSums[j], max);
-
-        return max;
+    private static int klSums(int[] a, int k, int l, int[] kMaxSums) {
+        int lSum = sliceSum(a, a.length - l, a.length - 1);
+        int maxSum = kMaxSums[a.length - l - 1] + lSum;
+        for (int start = a.length - l - 1; start > k; start--) {
+            lSum = lSum - a[start + l] + a[start];
+            maxSum = max(maxSum, kMaxSums[start - 2] + lSum);
+        }
+        return maxSum;
     }
 
-    private static int sliceSum(int[] a, int start, int length) {
+    private static int sliceSum(int[] a, int start, int end) {
         int sum = 0;
-        for (int i = start; i < start + length; i++)
+        for (int i = start; i <= end; i++)
             sum += a[i];
         return sum;
     }
