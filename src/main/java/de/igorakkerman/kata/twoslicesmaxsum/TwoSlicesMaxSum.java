16f0eb723bd1345c62f0df9eebc7ca95d955519c
Igor Akkerman <codingchallenge@igorakkerman.de>
2019-05-09 13:11:15 +0200

    kMaxSums array created and returned by kSums, klSums renamed to klMaxSum

--- src/main/java/de/igorakkerman/kata/twoslicesmaxsum/TwoSlicesMaxSum.java
+++ src/main/java/de/igorakkerman/kata/twoslicesmaxsum/TwoSlicesMaxSum.java
@@ -7,30 +7,34 @@ public class TwoSlicesMaxSum {
         if (a.length < k + l)
             return -1;
 
-        int[] kMaxSums = new int[a.length - l];
-
-        kSums(a, k, l, kMaxSums);
-        return klSums(a, k, l, kMaxSums);
+        return klMaxSum(a, k, l, kSums(a, k, l));
     }
 
-    private static void kSums(int[] a, int k, int l, int[] kMaxSums) {
+    private static int[] kSums(int[] a, int k, int l) {
+        int[] kMaxSums = new int[a.length - l];
+
         int kSum = sliceSum(a, 0, k - 1);
         kMaxSums[k - 1] = kSum;
+
         for (int upperBound = k; upperBound < a.length - l; upperBound++) {
             kSum = kSum - a[upperBound - k] + a[upperBound];
             kMaxSums[upperBound] = max(kMaxSums[upperBound - 1], kSum);
         }
+
+        return kMaxSums;
     }
 
-    private static int klSums(int[] a, int k, int l, int[] kMaxSums) {
+    private static int klMaxSum(int[] a, int k, int l, int[] kMaxSums) {
         int lowerBound = a.length - l;
         int lSum = sliceSum(a, lowerBound, a.length - 1);
         int maxSum = Integer.MIN_VALUE;
+
         do {
             --lowerBound;
             maxSum = max(maxSum, kMaxSums[lowerBound] + lSum);
             lSum = lSum - a[lowerBound + l] + a[lowerBound];
         } while (lowerBound >= k);
+
         return maxSum;
     }
 
